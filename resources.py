098f264fc2d5565595c21e53bc9f2af0ecfc8de2
alanapiccoli22-lang <alanapiccoli22@gmail.com>
2026-07-08 08:43:59 -0300

    Add files via upload

---
+++ resources.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Este arquivo serve como um espaco reservado (placeholder).
+# Para evitar a necessidade de compilacao manual (pyrcc5), o plugin carrega
+# os icones diretamente do sistema de arquivos usando caminhos relativos.
