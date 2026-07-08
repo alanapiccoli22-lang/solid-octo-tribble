fddc4d7a8b7a54e3c75eb3d6a72cc03ff474abf4
alanapiccoli22-lang <alanapiccoli22@gmail.com>
2026-07-08 08:28:26 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def classFactory(iface):
-    """Carrega a classe principal do plugin.
-    
-    :param iface: A interface do QGIS (QgisInterface)
-    """
-    from .dem_to_tactile_plugin import DemToTactilePlugin
-    return DemToTactilePlugin(iface)
