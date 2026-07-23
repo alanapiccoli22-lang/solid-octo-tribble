8366c0aa430b34d52636e8d9238b25019520a33e
alanapiccoli22-lang <alanapiccoli22@gmail.com>
2026-07-23 08:19:28 -0300

    Update dem_to_tactile_plugin.py

--- dem_to_tactile_plugin.py
+++ dem_to_tactile_plugin.py
@@ -1,14 +1,1 @@
-# Clone o repositório (se ainda não tiver localmente)
-git clone https://github.com/alanapiccoli22-lang/solid-octo-tribble.git
-cd solid-octo-tribble
 
-# Remove todos os arquivos existentes (exceto a pasta .git)
-git rm -rf *
-
-# Copie sua pasta nova para dentro deste diretório
-# (arraste os arquivos novos para cá, ou use cp/mv)
-
-# Adicione, comite e envie
-git add .
-git commit -m "Substitui arquivos antigos pela nova estrutura"
-git push
