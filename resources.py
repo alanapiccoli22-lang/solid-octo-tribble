9d425bc9e30e0610c0d9d1aed72bc0987ed53bae
alanapiccoli22-lang <alanapiccoli22@gmail.com>
2026-07-23 08:44:56 -0300

    Delete resources.py

--- resources.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# Este arquivo serve como um espaco reservado (placeholder).
-# Para evitar a necessidade de compilacao manual (pyrcc5), o plugin carrega
-# os icones diretamente do sistema de arquivos usando caminhos relativos.
