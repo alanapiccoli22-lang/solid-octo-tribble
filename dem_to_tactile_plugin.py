403ae6a24000f83a92caa73daf3551adb27f7e86
alanapiccoli22-lang <alanapiccoli22@gmail.com>
2026-07-23 08:19:08 -0300

    Update dem_to_tactile_plugin.py

--- dem_to_tactile_plugin.py
+++ dem_to_tactile_plugin.py
@@ -1,82 +1,14 @@
-# -*- coding: utf-8 -*-
+# Clone o repositório (se ainda não tiver localmente)
+git clone https://github.com/alanapiccoli22-lang/solid-octo-tribble.git
+cd solid-octo-tribble
 
-import os
-from qgis.PyQt.QtGui import QIcon
-from qgis.PyQt.QtWidgets import QAction
+# Remove todos os arquivos existentes (exceto a pasta .git)
+git rm -rf *
 
-# Importar o diálogo do plugin
-from .dem_to_tactile_dialog import DemToTactileDialog
+# Copie sua pasta nova para dentro deste diretório
+# (arraste os arquivos novos para cá, ou use cp/mv)
 
-
-class DemToTactilePlugin:
-    """Classe principal de integração do QGIS para o plugin DEM to Tactile 3D."""  # noqa: E501
-
-    def __init__(self, iface):
-        """Inicializa o plugin com a interface do QGIS.
-
-        :param iface: Instância da QgisInterface do QGIS.
-        """
-        self.iface = iface
-        self.plugin_dir = os.path.dirname(__file__)
-
-        # Referência ao diálogo principal
-        self.dialog = None
-
-        # Elementos da interface gráfica do QGIS
-        self.action = None
-        self.menu_title = "DEM to Tactile 3D"
-
-    def initGui(self):
-        """Inicializa os elementos de interface gráfica do plugin no QGIS."""
-        # Caminho para o ícone
-        icon_path = os.path.join(self.plugin_dir, 'icon.png')
-        icon = QIcon(icon_path)
-
-        # Criar a Ação do QGIS
-        self.action = QAction(
-            icon,
-            "Gerar Modelo 3D Tátil...",
-            self.iface.mainWindow()
-        )
-        self.action.setStatusTip(
-            "Transforma um MDE e vetores em um modelo 3D tátil fechado (STL)")
-        self.action.setWhatsThis(
-            "Plugin para geração de modelos físicos 3D táteis a partir de relevo raster e vetores.")  # noqa: E501
-        self.action.triggered.connect(self.run)
-
-        # Adicionar ao menu 'Raster' do QGIS
-        self.iface.addPluginToRasterMenu(self.menu_title, self.action)
-
-        # Adicionar à barra de ferramentas 'Raster' do QGIS
-        self.iface.addRasterToolBarIcon(self.action)
-
-    def unload(self):
-        """Remove os elementos de interface criados pelo plugin ao desativá-lo."""  # noqa: E501
-        if self.action:
-            # Remover do menu 'Raster'
-            self.iface.removePluginRasterMenu(self.menu_title, self.action)
-            # Remover da barra de ferramentas
-            self.iface.removeRasterToolBarIcon(self.action)
-
-        # Destruir o diálogo se ele existir
-        if self.dialog:
-            self.dialog.deleteLater()
-            self.dialog = None
-
-    def run(self):
-        """Executa a rotina principal do plugin abrindo o diálogo de configurações."""  # noqa: E501
-        # Criar o diálogo se for a primeira execução
-        if self.dialog is None:
-            self.dialog = DemToTactileDialog(
-                self.iface, self.iface.mainWindow())
-
-        # Mostrar o diálogo na tela
-        self.dialog.show()
-        # Trazer para frente se já estiver aberto
-        self.dialog.raise_
-        self.dialog.activateWindow()
-        # Compatibilidade PyQt5 / PyQt6 (exec_ foi removido no PyQt6)
-        if hasattr(self.dialog, 'exec'):
-            self.dialog.exec()
-        else:
-            self.dialog.exec_()
+# Adicione, comite e envie
+git add .
+git commit -m "Substitui arquivos antigos pela nova estrutura"
+git push
